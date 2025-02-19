f6b6d6af86a49605093d9a426fbfb6c02eea0670
ShcherbinaNick <scherbinanick@yandex.ru>
2025-02-19 22:15:46 +0300

    5th lesson: useeffect, mockapi

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import React from 'react'
 import './scss/app.scss';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
 
 function App() {
+
+  const [ pizzas, setPizzas ] = React.useState([]);
+  
+  React.useEffect(() => {
+    fetch('https://67b5a50207ba6e59083dcc60.mockapi.io/pizzas')
+    .then(res => res.json())
+    .then((pizzasArr) => {
+      setPizzas(pizzasArr)
+    })
+  }, [])
+
   return (
     <div className="wrapper">     
      <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
@@ -20,7 +31,7 @@ function App() {
             {
               pizzas.map((pizza) => 
                 <PizzaBlock key={ pizza.id } { ...pizza } />
-              )
+            )
             }
           </div>
         </div>
